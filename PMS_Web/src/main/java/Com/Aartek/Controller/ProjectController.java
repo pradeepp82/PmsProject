a9ee899eec082a5edc3808c4b568aa880cb81511
pradeepp82 <30548046+pradeepp82@users.noreply.github.com>
2017-08-17 13:51:15 +0530

    Update ProjectController.java

--- PMS_Web/src/main/java/Com/Aartek/Controller/ProjectController.java
+++ PMS_Web/src/main/java/Com/Aartek/Controller/ProjectController.java
@@ -39,7 +39,7 @@ public class ProjectController {
 	public String Addproject(Map<String, Object> map,Model model) {
 		
 		
-		System.out.println("inside controller Addproject by git ");
+		System.out.println("inside controller Addproject by git  pp");
 		map.put("project", new Project());
 		
 		List<Project> projectList = proservice.getProjects();
